4b56c5fea172d42ca83fe362c7b36f023ff1f8ea
TrymG <trymgall@gmail.com>
2026-08-16 23:17:03 +0200

    added login request and response dto

---
+++ src/main/java/com/jobtracker/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.jobtracker.dto;
+
+public class LoginResponse {
+
+    private String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+    
+}
